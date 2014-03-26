fd5a1bb9a15b3b8800fd77a9ffe749e6b2470f7e
megalord <jwgriege@sbcglobal.net>
2014-03-25 20:26:27 -0500

    Several little fixes; actually works now. Minified script added and readme updated.

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-spider
-======
+#spider
 
-A library for defining and injecting client-side modules from different files
+A library for defining and injecting client-side modules from different files.
+
+####config(newSettings)
+
+Writes each key/value pair of the passed object literal to the private settings object.
+
+Available settings:
+* basePath (string) = '/' -> The base path (root folder) of the javascript files on the server.
+* concat (bool) = false -> A flag indicating if the javascript files for the modules are concatenated. When false, no attempt is made to load dependencies from the server.
+
+####define(module, constructor)
+
+Define a module with a constructor function, which can return any data type. The constructor function is only executed once, no matter how many times the module is imported across other modules. The functions result is cached in a registry, and that variable is returned for subsequent imports.
+
+All modules should be named by their file path relative to the basePath.  The .js file extension should not be in the name, nor should a leading backslash.  For example, /app/main.js should be named 'app/main'.
+
+Spider will automatically attempt to load any modules imported by the constructor.
+
+####execute(module)
+
+Tells spider which module is the root module.  If the module dependencies are visualized as a web, this module is the center.  Once the entire module web is loaded, the constructor function for the specified module is executed.  The execute method returns the result of the constructor, just like import.
+
+```javascript
+spider.execute('app');  // loads app.js, which should have a call to spider.define()
+```
+
+####import(module)
+
+Imports a module, returning whatever is returned by that module's constructor function. Note that calls to import outside of a define constructor will fail unless the module being imported is already loaded.
+
+```javascript
+spider.define('model', function() {
+    return {
+        a:1
+    };
+});
+
+var model = spider.import('model');
+// model.a returns 1
+```
--- spider.js
+++ spider.js
@@ -1,155 +1,157 @@
-spider = (function() {
+window.spider = (function() {
 
     // General note:
-    // a module's name is the same as its path minus what is set in the private basePath property
-    // this is also the first argument of the public define(), import(), and execute() methods
+    // A module's name is the same as its path minus what is set in the private basePath property.
+    // This is also the first argument of the public define(), fetch(), and execute() methods.
  
-        // storage for the modules' names that need to be executed as threads
+        // Keep a list of the modules' names that need to be executed when ready.
     var executeOnReady = {},
 
-        // storage for the modules' information
+        // This is where the module and their metadata is stored.
         registry = {},    
 
         settings = {
-            // the base path to the modules
-            basePath:'/js/',
+            // This is the base path to the modules.
+            basePath:'/',
 
-            // a setting for indicating if the modules are all defined in the same file
-            // should be set to false until all the js is concatenated
-            concat:false
+            // Indicates if the modules are all defined in the same file.
+            // This should be set to false until all the js is concatenated.
+            concat:false,
+
+            // Indicates if the files are being served from a server or the local file system.
+            local:false
         },
 
     config = function(newSettings) {
-        for(var option in newSettings) {
+        for(var option in newSettings)
             settings[option] = newSettings[option];
-        };
     },
 
-    // print the registry in the console for testing
-    debug = function() {
-        console.log(registry);
-    },
-
-    // define a module using a constructor function
-    // if a module is imported before it is defined,
-    //   then it has been added to the registry,
-    //   and it's importedBy property will not be empty,
-    //   and it's ready property will be false
+    // Define a module using a constructor function.
+    // If a module is imported before it is defined,
+    // then it has been added to the registry,
+    // and its importedBy property will not be empty,
+    // and its ready property will be false.
     define = function(module, constructor) {
         if(typeof registry[module] === 'undefined') {
-            // module was not imported by any previously defined modules
+            // The module was not imported by any previously defined modules.
             registry[module] = {
-                importedBy:[]
+                importedBy:[],
+                ready:false
             };
         };
-        // add more information pertinent to the module
+
+        // Add more information pertinent to the module.
         var dependencies = findDependencies(constructor);
         registry[module].constructor = constructor;
         registry[module].dependencies = dependencies;
-        // register each of the module's dependencies, loading them if needed
-        for(var i = 0, ilen = dependencies.length; i < ilen; i++) {
+
+        if(dependencies.length === 0)
+            registry[module].ready = true;
+
+        // Register each of the module's dependencies, loading them if needed.
+        for(var i = 0; i < dependencies.length; i++) {
             if(typeof registry[dependencies[i]] === 'undefined') {
-                // dependency module was not imported by any previously defined modules
+                // The dependency module was not imported by any previously defined modules.
                 registry[dependencies[i]] = {
                     importedBy:[],
                     ready:false
                 };
-                if(!settings.concat) {
-                    load(dependencies[i]);
-                };
+
+                load(dependencies[i]);
             };
-            // the module name is saved in each dependency module so that
-            //   readiness can propagate up the module tree
+
+            // The module name is saved in each dependency module so that
+            // readiness can propagate up the module tree
             registry[dependencies[i]].importedBy.push(module);
         };
+
         propagateReadiness(module);
     },
 
-    // build the application once all modules are resolved
+    // Mark a module as being needed to run when it is ready.
     execute = function(module) {
-        if(typeof registry[module] !== 'undefined' && registry[module].ready) {
-            registry[module].constructor();
-        }else{
-            executeOnReady[module] = true;
-            if(!settings.concat) {
-                load(module);
-            };
-        };
+        if(module in registry && registry[module].ready)
+            return registry[module].constructor();
+
+        executeOnReady[module] = true;
+        load(module);
     },
 
-    // determine a module's dependencies by looking for the module.import() method
-    //   and extracting the argument
-    // returns an array of module modules
+    // Import a module into the scope.
+    // This method returns the result of the module's constructor
+    // and should only be called inside the constructor passed to the define() method.
+    fetch = function(module) {
+        if(typeof registry[module].obj === 'undefined')
+            registry[module].obj = registry[module].constructor();
+
+        return registry[module].obj;
+    },
+
+    // Determine a module's dependencies by looking for the module.import() method
+    // in its constructor function and extracting the argument.
+    // Returns an array of module names.
     findDependencies = function(constructor) {
-        var dependencies = [],
+        var match,
+            dependencies = [],
             constructorString = constructor.toString(),
-            regex = /spider\.import\('([\w\/]*)'\)/g,
-            match = regex.exec(constructorString);
-        while(match) {
+            regex = /spider\.fetch\('([\w\/]*)'\)/g;
+
+        while(match = regex.exec(constructorString))
             dependencies.push(match[1]);
-            match = regex.exec(constructorString);
-        };
+
         return dependencies;
     },
     
-    // load a script from the server in a new script tag
-    // delete the script tag once the js has been parsed
+    // Load a script from the server in a new script tag.
+    // Delete the script tag once the js has been parsed.
     load = function(module) {
+        if(settings.concat)
+            return;
+
         var script = document.createElement('script');
-        script.src = settings.basePath + module + '.js';
+        script.src = (settings.local ? '' : settings.basePath) + module + '.js';
         script.addEventListener('load', function() {
             script.parentNode.removeChild(script);
         });
         document.getElementsByTagName('head')[0].appendChild(script);
     },
 
-    // resolve the readiness of any modules that import the given module
-    // will atttempt to propagate up the module tree
-    // also checks ready modules against the list of modules to be executed when ready,
-    //   as are determined by the module.exec() method
+    // Resolve the readiness of any modules that import the given module.
+    // This method will atttempt to propagate up the module tree.
+    // It also checks ready modules against the list of modules to be executed when ready,
+    // as are determined by the module.execute() method.
     propagateReadiness = function(module) {
-        var ready = testReadiness(module);
-        registry[module].ready = ready;
-        if(ready) {
+        if(!registry[module].ready)
+            registry[module].ready = testReadiness(module);
+
+        if(registry[module].ready) {
             if(executeOnReady[module]) {
                 delete executeOnReady[module];
                 registry[module].constructor();
-            }else{
+            } else {
                 var requestingModules = registry[module].importedBy;
-                for(var i = 0, ilen = requestingModules.length; i < ilen; i++) {
+                for(var i = 0; i < requestingModules.length; i++)
                     propagateReadiness(requestingModules[i]);
-                };
             };
         };
     },
 
-    // look at the readiness of all the dependencies of a module
-    // return true if all dependencies are ready, false otherwise
+    // Check the readiness of all the dependencies of a module.
+    // Returns true if all dependencies are ready, false otherwise.
     testReadiness = function(module) {
         var dependencies = registry[module].dependencies;
-        for(var i = 0, ilen = dependencies.length; i < ilen; i++) {
-            if(!registry[dependencies[i]].ready) {
+        for(var i = 0; i < dependencies.length; i++)
+            if(!registry[dependencies[i]].ready)
                 return false;   
-            };
-        };
         return true;
     };
     
     return {
         config:config,
-        debug:debug,
         define:define,
         execute:execute,
-
-        // import a module into the app
-        // return the module object
-        // can only be called inside the constructor passed to the define() method
-        import:function(module) {
-            if(typeof registry[module].obj === 'undefined') {
-                registry[module].obj = registry[module].constructor();
-            };
-            return registry[module].obj;
-        }
+        fetch:fetch
     };
 
 }());
